fbac4eb8797f82680397f6f329304a5a853b29e8
ChonkySeals <trivm182003@gmail.com>
2024-11-24 14:44:39 +0700

    change header detail

--- src/app/components/Customer/DetailScreen/DetailOpts.tsx
+++ src/app/components/Customer/DetailScreen/DetailOpts.tsx
@@ -1,19 +1,24 @@
 import { View } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { Bell, ShoppingCart } from "lucide-react-native";
+import { Bell } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+import { CustomerStackParamList } from "~/src/app/navigators/CustomerNavigators/CustomerNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 interface Props {
   color: string;
 }
 
 const DetailOpts = ({ color }: Props) => {
+  const navigation =
+    useNavigation<
+      NativeStackNavigationProp<CustomerStackParamList, "UserNotification">
+    >();
+
   return (
     <View style={{ flexDirection: "row", alignItems: "center" }} >
-      <TouchableOpacity style={{ marginRight: 15 }}>
-        <ShoppingCart size={24} color={color} />
-      </TouchableOpacity>
-      <TouchableOpacity style={{ marginRight: 10 }}>
+      <TouchableOpacity style={{ marginRight: 5 }} onPress={() => navigation.navigate("UserNotification")}>
         <Bell size={24} color={color} />
       </TouchableOpacity>
     </View>
